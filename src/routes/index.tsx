78c8a95fccb6c44ceecd3871574e5fcff3cd88dc
sudhanshu-niva <sudhanshu@nivamoney.com>
2024-11-11 23:41:08 +0530

    boiler plate

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "../pages/login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
