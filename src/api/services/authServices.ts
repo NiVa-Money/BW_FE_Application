a509f292b9370188e55c25f0d41936a57759fc0b
kalpanathmajhi <kalpanathmajhi@gmail.com>
2025-01-11 21:38:14 +0530

    added google login

--- src/api/services/authServices.ts
+++ src/api/services/authServices.ts
@@ -49,8 +49,43 @@ export const LoginUserService = async (payload: any) => {
       throw new Error('Error in verifying otp');
     }
   };
+//for google 
+  export const LoginverifyGoogleLogin = async (payload: any) => {
 
+    try {
+      const response = await axiosInstance.post('/user/signup', payload);
+  
+      // Extract data from response.body
+      const { user_id, token, orgName, roleName, moduleMap } = response.data.body;
+  
+      // Save to localStorage
+      localStorage.setItem('user_id', user_id);
+      localStorage.setItem('token', token);
+      localStorage.setItem('orgName', orgName);
+      localStorage.setItem('roleName', roleName);
+      localStorage.setItem('moduleMap', JSON.stringify(moduleMap));
+  
+  
+      return response.data;
+  
+    } catch (error: any) {
+      const errorMessage =
+        error?.response?.data?.error || 'Login failed. Please try again.';
+      throw new Error(errorMessage);
+    }
+  };
   
 
 
+  export const verifyGoogleUserService = async (payload: any) => {
+    try {
+      const response = await axiosInstance.post(
+        '/user/signup/verify',
+        payload
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error('Error in verifying otp');
+    }
+  };
 
