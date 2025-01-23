d63cfddad314df5e44d2277745950731d1d137a9
sneha-jhaveri <sneha@nivamoney.com>
2025-01-23 15:08:30 +0530

    bug fix

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,6 +1,8 @@
 import { takeEvery, takeLatest } from 'redux-saga/effects';
 import { CREATE_BOT } from '../actionTypes/botActionsTypes';
 import { createBotSaga } from './botSagas';
+import { CREATE_WHATSAPP_CAMPAIGN } from '../actionTypes/whatsappCampaignTypes';
+import { createWhatsAppCampaignSaga } from './whatsappCampaignSaga';
 
 export default function* rootSaga() {
     yield takeEvery(CREATE_BOT, createBotSaga);
