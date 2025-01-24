48cca8204f02ae399a71fb1e527cf0bc381e6d2d
batrasudhanshu <batrasudhanshu09@gmail.com>
2025-01-24 12:32:28 +0530

    botservice

--- src/api/services/botService.ts
+++ src/api/services/botService.ts
@@ -3,18 +3,54 @@ import { notifyError, notifySuccess } from "../../components/Toast";
 import axiosInstance from "../axiosConfig";
 
 export const createBotProfileService = async (payload: any) => {
-    try {
-      const response = await axiosInstance.post(
-        '/user/createBotProfile',
-        payload
-      );
-      return response.data;
-      notifySuccess('Bot Created Successfully')
-    } catch (error) {
-      // console.log('e',error)
-      notifyError('error')
-      
-    toast.error('error')
-      throw new Error('Error in Creating Bot');
-    }
-  };
+  try {
+    const response = await axiosInstance.post(
+      "/user/createBotProfile",
+      payload
+    );
+    return response.data;
+    notifySuccess("Bot Created Successfully");
+  } catch (error) {
+    // console.log('e',error)
+    notifyError("error");
+
+    toast.error("error");
+    throw new Error("Error in Creating Bot");
+  }
+};
+
+export const editBotProfileService = async (payload: any) => {
+  try {
+    const response = await axiosInstance.post("/user/editBotProfile", payload);
+    return response.data;
+  } catch (error) {
+    // console.log('e',error)
+
+    throw new Error("Error in Creating Bot");
+  }
+};
+
+export const getBotsService = async (payload: any) => {
+  try {
+    const response = await axiosInstance.get(
+      `user/getUserBotProfiles?userId=${payload}`,
+      {}
+    );
+    return response.data;
+  } catch (error: any) {
+    console.log("e", error);
+    throw new Error("Error fetching user profile");
+  }
+};
+export const deleteBotService = async (payload: any) => {
+  try {
+    const response = await axiosInstance.get(
+      `user/deleteBotProfile?botId=${payload.botId}&userId=${payload.userId}`,
+      {}
+    );
+    return response.data;
+  } catch (error: any) {
+    console.log("e", error);
+    throw new Error("Error Deleting Bot profile");
+  }
+};
