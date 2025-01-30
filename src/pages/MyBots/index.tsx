d80a9f3c9608160679634688c01e851129396f27
sneha-jhaveri <sneha@nivamoney.com>
2025-01-30 11:50:34 +0530

    merge code

--- src/pages/MyBots/index.tsx
+++ src/pages/MyBots/index.tsx
@@ -1,67 +1,78 @@
-import React, { useEffect, useState } from "react";
-import MyBotCard from "./MyBotCard";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  deleteBotAction,
-  getBotsAction,
-  resetBotAction,
-} from "../../store/actions/botActions";
-import { RootState } from "../../store";
-import { formatDateString } from "../../hooks/functions";
-import { useNavigate } from "react-router-dom";
-import DeleteConfirmationModal from "../../components/DeleteConfirmationModal";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
+import MyBotCard from './MyBotCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteBotAction, getBotsAction, resetBotAction } from '../../store/actions/botActions';
+import { RootState } from '../../store';
+import { formatDateString } from '../../hooks/functions';
+import { useNavigate } from 'react-router-dom';
+import DeleteConfirmationModal from '../../components/DeleteConfirmationModal';
+
+
 
 const MyBots: React.FC = () => {
-  const dispatch = useDispatch();
-  const [botLists, setbotLists] = useState<any>([]);
-  const [userId, setUserId] = useState("");
-  const botsDataRedux = useSelector(
-    (state: RootState) => state.bot?.lists?.data
-  );
-  const deleteBotDataRedux = useSelector(
-    (state: RootState) => state.bot?.create?.data
-  );
-  const userIdLocal = localStorage.getItem("user_id");
+    const dispatch = useDispatch()
+    const [botLists, setbotLists] = useState<any>([])
+    const [userId, setUserId] = useState('')
+    const botsDataRedux = useSelector(
+        (state: RootState) => state.bot?.lists?.data
+    );
+    const deleteBotDataRedux = useSelector(
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+        (state: RootState) => state.bot?.create?.data
 
-  const [payloadDelete, setPayloadDelete] = useState({});
-  const navigate = useNavigate();
-  const botsDataLoader = useSelector(
-    (state: RootState) => state.bot?.lists?.loader
-  );
-  const createBotHandler = () => {
-    navigate("/createbot");
-  };
-  const handleOpen = (botId: string) => {
-    setIsModalOpen(true);
+    );
+    const userIdLocal = localStorage.getItem('user_id')
 
-    setPayloadDelete({
-      botId: botId,
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-      userId: userId,
-    });
-  };
-  const handleEdit = (id: string) => {
-    navigate(`/editbot/:${id}`);
-  };
-  const handleDelete = () => {
-    dispatch(deleteBotAction(payloadDelete));
-    setIsModalOpen(false);
-  };
-
-  const handleExport = () => {
-    console.log("Export action triggered!");
-  };
-
-  useEffect(() => {
-    if (deleteBotDataRedux !== null) {
-      const success = deleteBotDataRedux?.success;
-      if (success) {
-        dispatch(getBotsAction(userId));
-      }
+    const [payloadDelete, setPayloadDelete] = useState({})
+    const navigate = useNavigate();
+    const botsDataLoader = useSelector(
+        (state: RootState) => state.bot?.lists?.loader
+    );
+    const createBotHandler = () => {
+        navigate('/createbot')
     }
-  }, [deleteBotDataRedux]);
+    const handleOpen = (botId: string) => {
+
+        setIsModalOpen(true)
+
+        setPayloadDelete({
+
+            botId: botId,
+
+            userId: userId
+
+        })
+    }
+    const handleEdit = (id: string) => {
+
+        navigate(`/editbot/:${id}`)
+
+
+    };
+    const handleDelete = () => {
+        dispatch(deleteBotAction(payloadDelete))
+        setIsModalOpen(false);
+
+    };
+
+
+    const handleExport = () => {
+        console.log('Export action triggered!');
+    };
+
+    useEffect(() => {
+
+        if (deleteBotDataRedux !== null) {
+            const success = deleteBotDataRedux?.success
+            if (success) {
+                dispatch(getBotsAction(userId))
+            }
+        }
+
+    }, [deleteBotDataRedux])
 
     useEffect(() => {
         if (botsDataRedux?.length && !botsDataLoader) {
@@ -69,17 +80,12 @@ const MyBots: React.FC = () => {
         }
 
     }, [botsDataRedux, botsDataLoader])
-  useEffect(() => {
-    if (botsDataRedux?.length) {
-      setbotLists(botsDataRedux);
-    }
-  }, [botsDataRedux, botsDataLoader]);
 
-  useEffect(() => {
-    if (userId?.length) {
-      dispatch(getBotsAction(userId));
-    }
-  }, [userId]);
+    useEffect(() => {
+        if (userId?.length) {
+            dispatch(getBotsAction(userId))
+        }
+    }, [userId])
 
     useEffect(() => {
         if (userId?.length) {
@@ -95,17 +101,6 @@ const MyBots: React.FC = () => {
 
 
 
-  useEffect(() => {
-    if (userId?.length) {
-      dispatch(getBotsAction(userId));
-    }
-    dispatch(resetBotAction("create"));
-  }, []);
-  useEffect(() => {
-    if (userIdLocal?.length) {
-      setUserId(userIdLocal);
-    }
-  }, [userIdLocal]);
 
     return (
         <div className='flex flex-col w-[100%]'>
@@ -137,41 +132,6 @@ const MyBots: React.FC = () => {
 
 
     );
-  return (
-    <div className="flex flex-col w-[100%]">
-      <button
-        className="self-end bg-[#65558F] text-white p-1 w-[160px] rounded-[100px] mb-2"
-        onClick={createBotHandler}
-      >
-        Create Bot
-      </button>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {botLists.map((item: any) => (
-          <div
-            key={item._id} // Always add a key prop when mapping arrays
-            className="m-[15px] max-w-[50%] min-w-[300px] w-[40%] mx-auto my-0 flex justify-center items-center"
-          >
-            <MyBotCard
-              name={item?.botName}
-              description={item?.botIdentity}
-              tone={item?.botTone}
-              fileName={item?.docName}
-              color={item?.botColor}
-              createdAt={formatDateString(item?.createdAt)}
-              onDelete={() => handleOpen(item._id)}
-              onExport={handleExport}
-              onClick={() => handleEdit(item._id)}
-            />
-          </div>
-        ))}
-      </div>
-      <DeleteConfirmationModal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onDelete={handleDelete}
-      />
-    </div>
-  );
 };
 
 export default MyBots;
