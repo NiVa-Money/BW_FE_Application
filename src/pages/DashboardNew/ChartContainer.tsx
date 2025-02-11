50e0274d414b2cef8ff830d795136e0e1f5c0f63
Ashish Madan <ashish@veersatech.com>
2025-02-12 03:26:25 +0530

    Refactored Chart Container

--- src/pages/DashboardNew/ChartContainer.tsx
+++ src/pages/DashboardNew/ChartContainer.tsx
@@ -7,48 +7,52 @@ interface ChartContainerProps {
   isMultiple?: boolean;
   title?: string;
   component:
-    | React.ReactElement
-    | Array<{ id: number; title: string; component: React.ReactElement }>;
+    | React.ReactElement // Single chart component
+    | Array<{ id: number; title: string; component: React.ReactElement }>; // Multiple charts with titles
 }
 
-const ChartContainer: React.FC<ChartContainerProps> = (props) => {
-  if (props.isMultiple && Array.isArray(props.component)) {
-    // Multi-chart layout
-    return (
-      <Paper sx={{ p: 3, ...props.extraSX }}>
-        <Box >
-          {props.component.map((item, index) => (
-            <Box key={index} sx={{ flex: 1 }} className="mb-3">
-              <Typography variant="h6" component="h3">
-                {item.title}
-              </Typography>
-              <ResponsiveContainer width="100%">
-                {item.component}
-              </ResponsiveContainer>
-            </Box>
-          ))}
-        </Box>
-      </Paper>
-    );
-  }
-
-  // Single chart layout
-  return (
-    <Paper sx={{ p: 3, ...props.extraSX }}>
-      <Box sx={{ display: "flex", justifyContent: "space-between", mb: 1 }}>
-        {
+const ChartContainer: React.FC<ChartContainerProps> = ({
+  extraSX,
+  isMultiple,
+  title,
+  component,
+}) => {
+  const renderSingleChart = () => (
+    <Paper sx={{ p: 3, ...extraSX }}>
+      {title && (
+        <Box sx={{ display: "flex", justifyContent: "space-between", mb: 1 }}>
           <Typography variant="h6" component="h3">
-            {props.title}
+            {title}
           </Typography>
-        }
-      </Box>
+        </Box>
+      )}
       <Box sx={{ height: 300 }}>
         <ResponsiveContainer width="100%" height="100%">
-          {props.component as React.ReactElement}
+          {component as React.ReactElement}
         </ResponsiveContainer>
       </Box>
     </Paper>
   );
+
+  const renderMultipleCharts = () => (
+    <Paper sx={{ p: 3, ...extraSX }}>
+      {Array.isArray(component) &&
+        component.map((item) => (
+          <Box key={item.id} sx={{ flex: 1 }} className="mb-3">
+            <Typography variant="h6" component="h3">
+              {item.title}
+            </Typography>
+            <ResponsiveContainer width="100%">
+              {item.component}
+            </ResponsiveContainer>
+          </Box>
+        ))}
+    </Paper>
+  );
+
+  return isMultiple && Array.isArray(component)
+    ? renderMultipleCharts()
+    : renderSingleChart();
 };
 
 export default ChartContainer;
