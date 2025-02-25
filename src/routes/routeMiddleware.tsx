69d8b1500d9ea7b792d10ffb32d8ecf1b504bb10
Ashish Madan <madaansahaab777@gmail.com>
2025-02-25 13:53:21 +0530

    Removed log by personal account

--- src/routes/routeMiddleware.tsx
+++ src/routes/routeMiddleware.tsx
@@ -50,8 +50,6 @@ const RouteMiddleware: React.FC<AuthMiddlewareProps> = ({
   if (isProtected && !userId) {
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
-  console.log("pathname", pathname);
-
   const logOutHandler = () => {
     navigate("/");
     localStorage.clear();
