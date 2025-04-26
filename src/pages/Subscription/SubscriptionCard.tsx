5e46cc8d03ac3cd7d11579b29ffd3e7b3843eace
batrasudhanshu <batrasudhanshu09@gmail.com>
2025-04-26 18:39:50 +0530

    subscription card update

--- src/pages/Subscription/SubscriptionCard.tsx
+++ src/pages/Subscription/SubscriptionCard.tsx
@@ -79,6 +79,16 @@ const SubscriptionCard: React.FC<any> = ({
                             <p className="ml-2 text-gray-600 text-sm">${planInfo.meta?.setupFee} setup fee (one-time)</p>
                         </div>}
 
+                    {planInfo.meta?.extraCreditsPrice && planInfo.meta?.sessionLimit &&
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0 mt-1">
+                                <svg className="w-5 h-5 text-gray-600" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <p className="ml-2 text-gray-600 text-sm">${planInfo.meta?.extraCreditsPrice} per {planInfo.meta?.sessionLimit} additional credits</p>
+                        </div>}
+
                     {planInfo.meta?.extraUserPrice && planInfo.meta?.extraCreditsPerUser &&
                         <div className="flex items-center">
                             <div className="flex-shrink-0 mt-1">
